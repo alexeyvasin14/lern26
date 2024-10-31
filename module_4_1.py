c84d6697ae60f0738902f742a9b1c59fae0fe1ac
alexeyvasin14 <vasin14@mail.ru>
2024-10-31 20:08:50 +0300

    Add files via upload

---
+++ module_4_1.py
@@ -0,0 +1,13 @@
+from true_math import divid as div
+from fake_math import divid as vid
+fake_divide = vid
+true_divide = div
+
+result1 = fake_divide(69, 3)
+result2 = fake_divide(3, 0)
+result3 = true_divide(49, 7)
+result4 = true_divide(15, 0)
+print(result1)
+print(result2)
+print(result3)
+print(result4)
